14f1e56e02ab3a4749d0b000bd13585f9cfe7e55
nealSchneier <neal.schneier@gmail.com>
2015-02-08 00:04:10 -0500

    models

--- mine/myApp/models.py
+++ mine/myApp/models.py
@@ -11,6 +11,62 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class AuthGroup(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(unique=True, max_length=80)
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+class AuthGroupPermissions(models.Model):
+    id = models.IntegerField(primary_key=True)
+    group = models.ForeignKey(AuthGroup)
+    permission = models.ForeignKey('AuthPermission')
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+
+class AuthPermission(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    content_type = models.ForeignKey('DjangoContentType')
+    codename = models.CharField(max_length=100)
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+
+class AuthUser(models.Model):
+    id = models.IntegerField(primary_key=True)
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField()
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=75)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+class AuthUserGroups(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(AuthUser)
+    group = models.ForeignKey(AuthGroup)
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+
+class AuthUserUserPermissions(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(AuthUser)
+    permission = models.ForeignKey(AuthPermission)
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+
 class Companies(models.Model):
     company_id = models.IntegerField()
     day_price_change = models.DecimalField(max_digits=30, decimal_places=3, blank=True, null=True)
@@ -25,30 +81,54 @@ class Companies(models.Model):
     curr_date = models.DateTimeField(blank=True, null=True)
     id = models.IntegerField(primary_key=True)
     class Meta:
-        managed = True
+        managed = False
         db_table = 'companies'
-    def __unicode__(self):
-        return str(self.company_id) + " " + str(self.curr_date)
 
 class Company(models.Model):
     name = models.CharField(unique=True, max_length=70, blank=True)
     symbol = models.CharField(max_length=30, blank=True)
     id = models.IntegerField(primary_key=True)
     class Meta:
-        managed = True
+        managed = False
         db_table = 'company'
-    def __unicode__(self):
-        return self.name
 
 class Companytosector(models.Model):
     id = models.IntegerField(primary_key=True)
     industry_id = models.IntegerField()
     company_id = models.IntegerField()
     class Meta:
-        managed = True
+        managed = False
         db_table = 'companyToSector'
-    def __unicode__(self):
-        return str(self.industry_id) + " " + str(self.company_id)
+
+class DjangoAdminLog(models.Model):
+    id = models.IntegerField(primary_key=True)
+    action_time = models.DateTimeField()
+    user = models.ForeignKey(AuthUser)
+    content_type = models.ForeignKey('DjangoContentType', blank=True, null=True)
+    object_id = models.TextField(blank=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.IntegerField()
+    change_message = models.TextField()
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+class DjangoContentType(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+    class Meta:
+        managed = False
+        db_table = 'django_session'
 
 class Industries(models.Model):
     sector_number = models.IntegerField(blank=True, null=True)
@@ -65,20 +145,16 @@ class Industries(models.Model):
     curr_date = models.DateTimeField(blank=True, null=True)
     id = models.IntegerField(primary_key=True)
     class Meta:
-        managed = True
+        managed = False
         db_table = 'industries'
-    def __unicode__(self):
-        return str(self.industry) + " " + str(self.curr_date)
 
 class Industry(models.Model):
     name = models.CharField(max_length=50, blank=True)
     id = models.IntegerField(primary_key=True)
     sector_id = models.IntegerField(blank=True, null=True)
     class Meta:
-        managed = True
+        managed = False
         db_table = 'industry'
-    def __unicode__(self):
-        return self.name
 
 class Quotes(models.Model):
     company_id = models.IntegerField(blank=True, null=True)
@@ -92,19 +168,15 @@ class Quotes(models.Model):
     curr_date = models.DateTimeField(blank=True, null=True)
     id = models.IntegerField(primary_key=True)
     class Meta:
-        managed = True
+        managed = False
         db_table = 'quotes'
-    def __unicode__(self):
-        return str(self.company_id) + " " + str(self.quote_date)
 
 class Sector(models.Model):
     name = models.CharField(max_length=30, blank=True)
     id = models.IntegerField(primary_key=True)
     class Meta:
-        managed = True
+        managed = False
         db_table = 'sector'
-    def __unicode__(self):
-        return self.name
 
 class Sectors(models.Model):
     sector = models.CharField(max_length=50, blank=True)
@@ -120,8 +192,6 @@ class Sectors(models.Model):
     curr_date = models.DateTimeField(blank=True, null=True)
     id = models.IntegerField(primary_key=True)
     class Meta:
-        managed = True
+        managed = False
         db_table = 'sectors'
-    def __unicode__(self):
-        return self.sector + " " + str(self.curr_date)
 
